e21b3ef40a4cc8858886a41917b1719479db27f6
kankanalapoojitha <poojithakankanala22@gmail.com>
2024-07-18 21:39:41 +0530

    lab 1

---
+++ ThirdProgram.java
@@ -0,0 +1,26 @@
+package firstproject;
+
+public class ThirdProgram {
+
+	public static void main(String[] args) {
+		// TO display 15 sentences to describe about the architecture of JVM
+		System.out.println("JVM stands from Java virtual Machine.");
+		System.out.println("It is responsible for executing java bytecode.");
+		System.out.println("JVM provides the runtime eniveronment ffor java applications.");
+		System.out.println("It is software based engine.");
+		System.out.println("It acts as an interface between java applications and the hardware.");
+		System.out.println("JVM is platorm independent allowing java programs to run on any device.");
+		System.out.println("JVM implemention has a operating system.");
+		System.out.println("It consists os three main subsystems:class loader,Runtime Data area and execution.");
+		System.out.println("class loader substem loads classes and interfaces.");
+		System.out.println("Runtime data area is where data and instructions used in the JVM.");
+		System.out.println("Execution Engine executes byte code instructions used in the JVM.");
+		System.out.println("JVM performs various built in libraries and APIs for performing tasks.");
+		System.out.println("It supports multi-threading,allowing java programs to execute.");
+		System.out.println("JVM can optimize perormance through Just-in -Time(JIT).");
+		System.out.println("The JVM specification is defined by Oracle.");
+
+
+	}
+
+}
