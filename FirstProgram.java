db153f871aed9604de5c703558ec04c7bcc779cb
kankanalapoojitha <poojithakankanala22@gmail.com>
2024-07-18 21:38:10 +0530

    Add files via upload
    lab 1

---
+++ FirstProgram.java
@@ -0,0 +1,20 @@
+package firstproject;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TO display 10 sentences about myself
+		System.out.println("My name is Poojitha.");		
+		System.out.println("I am native to Guntur.");
+		System.out.println("Currently i am pursuing MCA.");
+		System.out.println("I am a quick learner.");
+		System.out.println("I belong to a nuclear family.");
+		System.out.println("I have one sibling.");
+		System.out.println("My hobbies are listening music and Reading books.");
+		System.out.println("My short term and long term goal is to get better jod in MNC companies.");
+		System.out.println("I enjoy cooking and experimenting with different recipes.");
+		System.out.println("I enjoy leraning new things.");
+
+	}
+
+}
