e72af6a1ef87a6806a81d9916d7b15038bcffb9f
kankanalapoojitha <poojithakankanala22@gmail.com>
2024-07-18 21:40:26 +0530

    lab 2

---
+++ Fourthprogram.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class Fourthprogram {
+
+	public static void main(String[] args) {
+		//  Write a java program to calculate simple interest.
+		float p,r,t,si;// principal amount,rate,time and simple interest respectively.
+		p = 13000; r =12; t=2;
+		si =(p*r*t)/100;
+		System.out.println("Simple Interest is:" +si);
+
+	}
+
+}
