10624d8d6503c595b3a81b8be20420d3d9e309da
kankanalapoojitha <poojithakankanala22@gmail.com>
2024-07-18 21:41:12 +0530

    lab 2

---
+++ Triangle.java
@@ -0,0 +1,23 @@
+package firstproject;
+//Write a program to input any three angles and check they form a triangle or not.
+public class Triangle {
+    public static void main(String[] args) 
+    {
+    	int a=80;
+    	int b=10;
+    	int c=50;
+    	int d=a+b+c;
+    	if(d==150)
+    	{
+    		System.out.println("It is a triangle");
+    	}
+    	else
+    	{
+    		System.out.println("It is not a triangle");
+    	}
+    
+		
+
+	}
+
+}
