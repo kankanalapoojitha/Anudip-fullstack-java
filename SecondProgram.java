a2877f9652a17116e97a718796915bd55a665c61
kankanalapoojitha <poojithakankanala22@gmail.com>
2024-07-18 21:39:01 +0530

    lab 1

---
+++ SecondProgram.java
@@ -0,0 +1,19 @@
+package firstproject;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TO display 10 sectences about computer 
+		System.out.println("Computers are electronic devices.");
+		System.out.println("Computers run on hardware and software.");
+		System.out.println("Computers process information.");
+		System.out.println("It can be laptop,desktop etc.");
+		System.out.println("They come in different sizes and types.");
+		System.out.println("The data is stored in memory.");
+		System.out.println("In computer internet connectivity is  a key feature.");
+		System.out.println("They can communicate with other devices." );
+		System.out.println("Computers are essential tool in todays digitalworls.");
+		System.out.println("Computers are essential in today's worls.");
+	}
+
+}
